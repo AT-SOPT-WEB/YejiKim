1abd045dd61f18ce66cd74b9ef2303dd7f36107f
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-04-12 17:37:51 +0900

    feat: 2주차 세미나

---
+++ week2/seminar/2-2/index.js
@@ -0,0 +1,29 @@
+
+const input = document.querySelector('.todo-input');
+const addBtn = document.querySelector('.add-btn');
+const todoList = document.querySelector('.todo-list');
+
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+todos.forEach((todo) => {
+    addTodo(todo);
+});
+
+addBtn.addEventListener('click', () => {
+    const value = input.value;
+
+    if (!value) return;
+
+    addTodo(value);
+
+    todos.push(value);
+    localStorage.setItem("todos", JSON.stringify(todos));
+
+    input.value = "";
+});
+
+function addTodo(todo) {
+    const li = document.createElement("li");
+    li.textContent = todo;
+    todoList.appendChild(li);
+}
