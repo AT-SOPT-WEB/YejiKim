cd78d1d32ebc75cf476b88d88ca8ffa76919ec5b
mnbvcxzyj <mnbvcxzyj@gmail.com>
2025-05-12 21:50:23 +0900

    feat: 메뉴바 반응형 구현

--- week4/assignment/week4-assignment/src/shared/components/Header/index.tsx
+++ week4/assignment/week4-assignment/src/shared/components/Header/index.tsx
@@ -1,40 +1,59 @@
 import { Link } from 'react-router';
 import * as styles from './style.css';
-import { User } from 'lucide-react';
+import { Menu, User, X } from 'lucide-react';
 import { useMyNickname } from '../../../api/user/hooks/useMyNickname';
 import { STORAGE_KEY } from '../../constants/storageKey';
+import { useState } from 'react';
+import clsx from 'clsx';
 
 function Header() {
   const { data, isLoading, error } = useMyNickname();
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
 
   const handleLogout = () => {
     localStorage.removeItem(STORAGE_KEY.USER_ID);
     location.href = '/';
   };
 
+  const handleMenuClick = () => setMenuOpen((prev) => !prev);
+
   const nickname = data?.data?.nickname;
 
   return (
     <header className={styles.header}>
-      <nav>
-        <ul className={styles.navList}>
-          <li className={styles.navItem}>
-            <Link to="/mypage/info">내 정보</Link>
-          </li>
-          <li className={styles.navItem}>
-            <Link to="/mypage/search">SOPT 회원 조회하기</Link>
-          </li>
-          <li className={styles.navItem}>
-            <button onClick={handleLogout}>로그아웃</button>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.topWrapper}>
+        <div className={styles.navAndMenu}>
+          <div className={styles.menuWrapper}>
+            <button className={styles.menuButton} onClick={handleMenuClick}>
+              {menuOpen ? (
+                <X className={styles.menuIcon} aria-label="메뉴 닫기" />
+              ) : (
+                <Menu className={styles.menuIcon} aria-label="메뉴 열기" />
+              )}
+            </button>
+          </div>
 
-      <div className={styles.userInfo}>
-        <User className={styles.userIcon} />
-        <span className={styles.userName}>
-          {isLoading ? '불러오는 중...' : error ? '에러 발생' : nickname}
-        </span>
+          <nav className={clsx(styles.nav, menuOpen && styles.navOpen)}>
+            <ul className={styles.navList}>
+              <li className={styles.navItem}>
+                <Link to="/mypage/info">내 정보</Link>
+              </li>
+              <li className={styles.navItem}>
+                <Link to="/mypage/search">SOPT 회원 조회하기</Link>
+              </li>
+              <li className={styles.navItem}>
+                <button onClick={handleLogout}>로그아웃</button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div className={styles.userInfo}>
+          <User className={styles.userIcon} />
+          <span className={styles.userName}>
+            {isLoading ? '불러오는 중...' : error ? '에러 발생' : nickname}
+          </span>
+        </div>
       </div>
     </header>
   );
--- week4/assignment/week4-assignment/src/shared/components/Header/style.css.ts
+++ week4/assignment/week4-assignment/src/shared/components/Header/style.css.ts
@@ -2,27 +2,96 @@ import { style } from '@vanilla-extract/css';
 
 export const header = style({
   display: 'flex',
+  flexDirection: 'column',
+  backgroundColor: '#3d71ae',
+  padding: '20px',
+  transition: 'all 0.3s ease',
+});
+
+export const navAndMenu = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '1rem',
+});
+
+export const topWrapper = style({
+  display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
-  padding: '30px 20px',
-  backgroundColor: '#3d71ae',
+  flexWrap: 'wrap',
+  width: '100%',
+});
+
+export const menuWrapper = style({
+  display: 'none',
+  '@media': {
+    '(max-width: 768px)': {
+      display: 'block',
+    },
+  },
+});
+
+export const menuButton = style({
+  background: 'none',
+  border: 'none',
+  cursor: 'pointer',
+});
+
+export const menuIcon = style({
+  width: '24px',
+  height: '24px',
+  color: '#fff',
+});
+
+export const nav = style({
+  overflow: 'hidden',
+  maxHeight: '0',
+  opacity: 0,
+  transform: 'translateY(-10px)',
+  transition: 'all 0.3s ease',
+
+  '@media': {
+    '(min-width: 769px)': {
+      display: 'flex',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+      gap: '1rem',
+      maxHeight: 'none',
+      opacity: 1,
+      transform: 'none',
+    },
+  },
+});
+
+export const navOpen = style({
+  maxHeight: '300px',
+  opacity: 1,
+  transform: 'translateY(0)',
 });
 
 export const navList = style({
   display: 'flex',
-  alignItems: 'center',
+  flexDirection: 'column',
   gap: '1rem',
-  cursor: 'pointer',
+
+  '@media': {
+    '(min-width: 769px)': {
+      flexDirection: 'row',
+    },
+  },
 });
 
 export const navItem = style({
   fontSize: '1.2rem',
   color: '#fff',
+  cursor: 'pointer',
 });
 
 export const userInfo = style({
   display: 'flex',
   alignItems: 'center',
+  color: '#fff',
+  gap: '0.5rem',
 });
 
 export const userIcon = style({
